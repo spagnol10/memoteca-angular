596691ed431b46095e8d8baec3e795195c751d8b
wellynton.spagnol <wellyntonspagnol10@gmail.com>
2024-10-07 11:43:19 -0300

    Paginação e HttpParams refactor

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
 
@@ -10,17 +10,18 @@ export class PensamentoService {
 
   private readonly API = 'http://localhost:3000/pensamentos'
 
-  
-
   constructor(private http: HttpClient) { }
 
   listar(page:number): Observable<Pensamento[]> {
     const limit = 6;
-
-
     // GET /posts?_page=1&_per_page=25
-    return this.http
-    .get<Pensamento[]>(`${this.API}?_page=${page}&_limit=${limit}`)
+
+    let params = new HttpParams()
+    .set("_page", page)
+    .set("_limit", limit)
+
+    // return this.http.get<Pensamento[]>(`${this.API}?_page=${page}&_limit=${limit}`)
+    return this.http.get<Pensamento[]>(this.API, {params})
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
